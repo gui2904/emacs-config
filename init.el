fd10bb936f2cf54c892722e9093fe4b5479a9115
gui2904 <gfulco4@gmail.com>
2024-11-15 01:44:06 -0500

    adding backup files feature

--- init.el
+++ init.el
@@ -233,3 +233,12 @@
   (add-hook 'before-save-hook 'lsp-format-buffer)
   (add-hook 'rust-ts-mode-hook
           (lambda () (setq indent-tabs-mode nil))))
+
+;; backup files setup
+(setq backup-directory-alist `(("." . "./.saves")))
+(setq backup-by-copying t)
+
+(setq delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)
